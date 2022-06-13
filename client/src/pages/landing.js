77882f8592b7ac4ed02f71f0bcb19df8be766d60
LenaSkarlett <lena.skarlett@gmail.com>
2022-06-13 10:07:41 +0300

    Fix error

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './../components/header';
 import IntroducingTeam from './../components/introducing-team';
 import { useParams } from 'react-router-dom';
-import React from 'react';
 
 const Landing = () => {
   const { name } = useParams();
